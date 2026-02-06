6e710521362088e04cbbb9c71d57ebc67b491691
Gowgowt <gowgowthamp228@gmail.com>
2026-02-06 22:28:27 +0530

    basic while loop program

---
+++ basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+int secrtnumber=42;
+int use_number=0;
+cout<<"welcome to high ! and low Game"<<endl;
+while(secrtnumber != use_number){
+cout<<"Enter your gusse number :"<<endl;
+
+}
+return 0;
+
+}
